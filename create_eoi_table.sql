f790d48260be9bbafa1db647a6953b747615b6ae
ChanJayShen <j25044225@student.newinti.edu.my>
2026-06-10 14:55:58 +0800

    Added create table prompt for SQL.

---
+++ create_eoi_table.sql
@@ -0,0 +1,21 @@
+-- Tell the server which database to use (replace with your actual DB name)
+USE web_assgn2;
+
+-- Create the table
+CREATE TABLE eoi (
+    eoild INT AUTO_INCREMENT PRIMARY KEY,
+    job_ref_num CHAR(5) NOT NULL,
+    f_name VARCHAR(50) NOT NULL,
+    l_name VARCHAR(50) NOT NULL,
+    date_birth DATE NOT NULL,
+    gender VARCHAR(10) NOT NULL,
+    street_add VARCHAR(40) NOT NULL,
+    suburb_town VARCHAR(40) NOT NULL,
+    state CHAR(3) NOT NULL,
+    postcode CHAR(4) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    p_num VARCHAR(15) NOT NULL,
+    skills VARCHAR(200),
+    other_skills TEXT,
+    status ENUM('New', 'Current', 'Final') DEFAULT 'New' NOT NULL
+);
