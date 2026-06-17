4cd92a79386d334b4acfc178614c9aa440bfbb9d
ChanJayShen <j25044225@student.newinti.edu.my>
2026-06-18 00:31:50 +0800

    Modified header and added comments for login.php

--- logout.php
+++ logout.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 
-// Unset all active session global array parameters
+// Unset all active session 
 session_unset();
 
 // Clear and destroy the tracking token cookie structure on the server
 session_destroy();
 
-// Safely send the user back to the gatekeeper login layout
+// Safely send the user back to the login page
 header("Location: login.php");
 exit();
 ?>
